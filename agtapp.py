6df06edd9d3bdb671d501b4f0e10ae7a743423c7
Tunnox <chukwuemekaumunna@gmail.com>
2024-12-06 16:33:56 +0000

    Update agtapp.py

--- agtapp.py
+++ agtapp.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
+import pandas as pd
 import oracledb
+import os
 
 app = Flask(__name__)
 
-# Database connection details
+# Database connection
 def get_db_connection():
     return oracledb.connect(
         user='ADMIN', 
@@ -13,6 +15,10 @@ def get_db_connection():
         wallet_location="C:\\ChukProj\\config_dir", 
         wallet_password='gHpt468/Chuk.VGA.Lli.ps'
     )
+
+# Initialize DataFrames
+attendance_df = pd.DataFrame(columns=['FIRST_NAME', 'ATTENDANCE'])
+data_df = pd.DataFrame(columns=['FIRST_NAME', 'LAST_NAME', 'AGE', 'GENDER', 'DATE_OF_BIRTH', 'DATE_OF_ENTRY'])
 
 @app.route('/')
 def index():
@@ -24,69 +30,35 @@ def index():
     connection.close()
     return render_template('index.html', names=names)
 
-@app.route('/update', methods=['POST'])
-def update_record():
+@app.route('/take_attendance', methods=['POST'])
+def take_attendance():
+    first_name = request.form['first_name']
+    attendance = request.form['attendance']
+    attendance_df.loc[len(attendance_df)] = [first_name, attendance]
+    return redirect(url_for('index'))
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     age = request.form['age']
     gender = request.form['gender']
     dob = request.form['dob']
-    tag_number = request.form['tag_number']
-    attendance = request.form['attendance']
     date_of_entry = request.form['date_of_entry']
-
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("""
-        UPDATE AGT_CHILDREN_DATA_RECORDS
-        SET AGE = :age, GENDER = :gender, DATE_OF_BIRTH = :dob, TAG_NUMBER = :tag_number,
-            ATTENDANCE = :attendance, DATE_OF_ENTRY = :date_of_entry
-        WHERE FIRST_NAME = :first_name AND LAST_NAME = :last_name
-    """, {
-        'age': age,
-        'gender': gender,
-        'dob': dob,
-        'tag_number': tag_number,
-        'attendance': attendance,
-        'date_of_entry': date_of_entry,
-        'first_name': first_name,
-        'last_name': last_name
-    })
-    connection.commit()
-    cursor.close()
-    connection.close()
+    data_df.loc[len(data_df)] = [first_name, last_name, age, gender, dob, date_of_entry]
     return redirect(url_for('index'))
 
-@app.route('/add', methods=['POST'])
-def add_record():
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    age = request.form['age']
-    gender = request.form['gender']
-    dob = request.form['dob']
-    tag_number = request.form['tag_number']
-    attendance = request.form['attendance']
-    date_of_entry = request.form['date_of_entry']
+@app.route('/download_attendance', methods=['POST'])
+def download_attendance():
+    download_path = request.form['download_path']
+    attendance_df.to_excel(download_path + '/attendance.xlsx', index=False)
+    return send_file(download_path + '/attendance.xlsx', as_attachment=True)
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("""
-        INSERT INTO AGT_CHILDREN_DATA_RECORDS (FIRST_NAME, LAST_NAME, AGE, GENDER, DATE_OF_BIRTH, TAG_NUMBER, ATTENDANCE, DATE_OF_ENTRY)
-        VALUES (:first_name, :last_name, :age, :gender, :dob, :tag_number, :attendance, :date_of_entry)
-    """, {
-        'first_name': first_name,
-        'last_name': last_name,
-        'age': age,
-        'gender': gender,
-        'dob': dob,
-        'tag_number': tag_number,
-        'attendance': attendance,
-        'date_of_entry': date_of_entry
-    })
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return redirect(url_for('index'))
+@app.route('/download_data', methods=['POST'])
+def download_data():
+    download_path = request.form['download_path']
+    data_df.to_excel(download_path + '/data_records.xlsx', index=False)
+    return send_file(download_path + '/data_records.xlsx', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
